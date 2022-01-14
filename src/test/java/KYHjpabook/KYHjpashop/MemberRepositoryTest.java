96614f3b533a9f85d6e31300b27504b5a9fead43
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-14 23:02:56 +0900

    주석 오타 수정, get메소드 네이밍 변경

--- src/test/java/KYHjpabook/KYHjpashop/MemberRepositoryTest.java
+++ src/test/java/KYHjpabook/KYHjpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package KYHjpabook.KYHjpashop;
 
+import KYHjpabook.KYHjpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 // Junit4의 Runwith과 같은 기능을 하는 Junit5 어노테이션 [출처: https://hirlawldo.tistory.com/39 [도비의 블로그]]
-// 하지만 dlal `@SpringBootTest` 에는 `@ExtendWith(SpringExtension.class)`가 포함되어 있다. 그래서 생략 가능  [https://www.whiteship.me/springboot-no-more-runwith/]
+// 하지만 이미 `@SpringBootTest` 에는 `@ExtendWith(SpringExtension.class)`가 포함되어 있다. 그래서 생략 가능  [https://www.whiteship.me/springboot-no-more-runwith/]
 //@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MemberRepositoryTest {
@@ -24,7 +25,7 @@ class MemberRepositoryTest {
     public void testMember() throws Exception {
         // given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         // when
         Long saveId = memberRepository.save(member);
@@ -32,7 +33,7 @@ class MemberRepositoryTest {
 
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);  // findMember == member
         // 같은 transaction 안에서 저장을하고 조회를 하면, 영속성 컨텍스트가 같다. 같은 영속성 컨텍스트 안에서는 id값이 같으면 같은 객체임을 보장한다. 영속성 컨텍스트에서 객체를 반환한 것(따라서 select문도 나가지 않음)
         System.out.println("findMember == member = " + (findMember == member));
