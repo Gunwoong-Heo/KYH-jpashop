2cd2c761586b39f1aac3bff8d99d282459cdce22
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-14 23:02:18 +0900

    엔티티 추가

---
+++ src/main/java/KYHjpabook/KYHjpashop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package KYHjpabook.KYHjpashop.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/KYHjpabook/KYHjpashop/domain/OrderStatus.java
@@ -0,0 +1,6 @@
+package KYHjpabook.KYHjpashop.domain;
+
+public enum OrderStatus {
+    ORDER,
+    CANCEL
+}
