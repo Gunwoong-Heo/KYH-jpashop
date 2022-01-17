f732bdf41d1a9635345c22e80757c6035f389558
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-18 01:47:40 +0900

    Item 계열 추가

--- src/main/java/KYHjpabook/KYHjpashop/repository/ItemRepository.java
+++ src/main/java/KYHjpabook/KYHjpashop/repository/ItemRepository.java
@@ -19,7 +19,8 @@ public class ItemRepository {
         if(item.getId() == null) {
             em.persist(item);
         } else {
-            em.merge(item);
+            // 주의: 변경 감지 기능을 사용하면 원하는 속성만 선택해서 변경할 수 있지만, 병합을 사용하면 모든 속성이 변경된다. 병합시 값이 없으면 null 로 업데이트 할 위험도 있다. (병합은 모든 필드를 교체한다.)
+            Item merge = em.merge(item);  // `em.merge`의 파라미터로 넘겨지는 item이 영속성 컨텍스트에 관리되는 객체가 되는 것은아니고, em.merge()를 통해 반환되는 객체가 영속성 컨텍스트로 관리되는 객체이다.
         }
     }
 
--- src/main/java/KYHjpabook/KYHjpashop/service/ItemService.java
+++ src/main/java/KYHjpabook/KYHjpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package KYHjpabook.KYHjpashop.service;
 
+import KYHjpabook.KYHjpashop.domain.item.Book;
 import KYHjpabook.KYHjpashop.domain.item.Item;
 import KYHjpabook.KYHjpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,28 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+/*
+    // 변경 감지 기능 사용 (dirty checking)
+    @Transactional
+    public Item updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);  // findOne으로 가져온 객체는 영속성 컨텍스트에 추가됨 -> dirtyChecking의 대상
+        // 실무에서는 setter를 남발하기보다는 의미 있는 메소드를 통해서 데이터를 변경해야한다.
+        // 예시 : findItem.change(price, name, stockQuantity);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        // save() 메소드를 호출하여 객체를 저장하는 과정이 불필요하다.
+        // `@Transactional`에 의해서 transaction이 commit이 되고, JPA에 의해 영속성 컨텍스트 flush가 일어남
+        return findItem;
+    }
+*/
+    @Transactional
+    public void updateItem(Long id, String name, int price) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
     }
 
     public List<Item> findItems() {
