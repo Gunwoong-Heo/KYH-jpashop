ee4fdc7c16d83208d3d7a4beeaf247874926d708
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-18 17:01:49 +0900

    `MemberApiController` 추가

---
+++ src/main/java/KYHjpabook/KYHjpashop/api/MemberApiController.java
@@ -0,0 +1,34 @@
+package KYHjpabook.KYHjpashop.api;
+
+import KYHjpabook.KYHjpashop.domain.Member;
+import KYHjpabook.KYHjpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {  // Json 형식으로 온 데이터를 Member객체에 매핑해줌
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+}
