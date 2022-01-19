83ee84a111813f0dbeff330f967e41d7c41a987b
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-20 00:56:43 +0900

    `hibernate5Module` 추가

--- src/main/java/KYHjpabook/KYHjpashop/KyhJpashopApplication.java
+++ src/main/java/KYHjpabook/KYHjpashop/KyhJpashopApplication.java
@@ -1,7 +1,9 @@
 package KYHjpabook.KYHjpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KyhJpashopApplication {
@@ -12,6 +14,14 @@ public class KyhJpashopApplication {
 //		hello.setData("test");
 //		String data = hello.getData();
 //		System.out.println("data = " + data);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+//		return new Hibernate5Module();  // no옵션 기본 세팅으로는 Jackson 라이브러리를 사용해서 반환할때, 지연로딩은 무시하고 null로 채워넣고 반환하게
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);  // Json생성하는 시점에 강제로 lazyLoading을 파파박 해버림
+		return hibernate5Module;
 	}
 
 }
